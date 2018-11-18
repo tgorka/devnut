37f5cea2372a43b448092d2447a17663766ca795
Tomasz Górka <tgorka@users.noreply.github.com>
2018-11-18 18:26:44 +0100

    Update devnut.cmd

--- devnut.cmd
+++ devnut.cmd
@@ -1,5 +1,17 @@
 @echo off&setlocal ENABLEEXTENSIONS
 
+REM change directory if needed
 C:
 chdir %HOMEPATH%\devnut
-docker-compose up
+
+REM run docker compose
+REM docker-compose up
+
+REM run docker with idea open
+docker -it --rm ^
+       -m 4g ^
+       -v ./.X11-unix:/tmp/.X11-unix ^
+       -v .:/home/nut ^
+       -name devnut ^
+       tgorka/devnut ^
+       idea
