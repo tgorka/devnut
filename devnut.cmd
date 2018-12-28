dd6ff0f2b50ce0ee54ee45bc37392e0a8a691699
Tomasz Górka <tgorka@users.noreply.github.com>
2018-12-28 14:24:59 +0100

    Update devnut.cmd

--- devnut.cmd
+++ devnut.cmd
@@ -12,17 +12,42 @@ SET PROVIDER_PATH=%cd%
 SET CMD=zsh --login
 SET CPU=3.0
 SET MEM=4g
+SET TEMPLATE_VOLUME=devnut
+SET CHECK_UPDATES="1"
 REM %%%%%%%%%%%%%%%%%%%%%%%%
 REM %%%%% END CUSTOMIZE %%%%
 REM %%%%%%%%%%%%%%%%%%%%%%%%
-
-REM create new volume if not exists
-REM powershell -command "(docker volume ls | findstr %NAME% | Measure-Object -line).lines"
+REM another variables
+SET USER_NAME=dev
 SET VOLUME_EXIST="0"
-FOR /f "tokens=2" %%V IN ('docker volume ls') DO IF %NAME%==%%V SET VOLUME_EXIST="1"
-IF %VOLUME_EXIST%=="0" (
+SET COPY_FROM_TEMPLATE="0"
+
+REM make sure to have latest image
+IF %CHECK_UPDATES% == "1" docker pull tgorka/devnut
+
+REM create new volume if not exists and copy from the template
+FOR /F "tokens=2" %%V IN ('docker volume ls') DO IF %NAME%==%%V SET VOLUME_EXIST="1"
+IF %VOLUME_EXIST% == "0" (
        ECHO Volume "%NAME%" does not exist. Creating one.
        docker volume create --name %NAME% --driver local
+       SET COPY_FROM_TEMPLATE="1"
+)
+IF %COPY_FROM_TEMPLATE% == "1" (
+       ECHO Copy data from template volume "%TEMPLATE_VOLUME%" to "%NAME%".
+       docker run -i -t --rm --privileged ^
+              --hostname nut ^
+              --mount type=volume,src=%TEMPLATE_VOLUME%,dst=/home/%USER_NAME%/from ^
+              --mount type=volume,src=%NAME%,dst=/home/%USER_NAME%/to ^
+              --name %NAME%-initialize-volume ^
+              tgorka/devnut ^
+              sudo rsync --archive /home/%USER_NAME%/from/ /home/%USER_NAME%/to/
+       ECHO Setting rights on user "%USER_NAME%" for data in volume "%NAME%".       
+       docker run -i -t --rm --privileged ^
+              --hostname nut ^
+              --mount type=volume,src=%NAME%,dst=/home/%USER_NAME%/data ^
+              --name %NAME%-initialize-volume ^
+              tgorka/devnut ^
+              sudo chown --recursive %USER_NAME% /home/%USER_NAME%/data
 )
 
 REM run docker with idea open
@@ -30,7 +55,7 @@ docker run -i -t --rm --privileged ^
        --hostname nut ^
        --memory %MEM% ^
        --cpus %CPU% ^
-       --mount type=volume,src=%NAME%,dst=/home/dev ^
+       --mount type=volume,src=%NAME%,dst=/home/%USER_NAME% ^
        --volume %PROVIDER_PATH%:/mnt/host ^
        --name %NAME% ^
        tgorka/devnut ^
